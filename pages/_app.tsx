d92f408e5c3a184ca4bf953ecf58fec487217f39
Travis Doughty <travis.doughty@publicissapient.com>
2021-09-06 21:10:00 -0400

    moving styles down

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "@styles/globals.scss";
 import type { AppProps } from 'next/app'
+import "@styles/globals.scss";
 
 function Encounters({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
